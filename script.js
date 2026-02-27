07261726040cf84ec7bb33dc975e1a90f940fa22
Harika4252 <harikakorada4252@gmail.com>
2026-02-27 12:40:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerText =
+        "I admire Sundar Pichai because his journey shows how hard work and education can lead to success. He rose from a modest background to become the CEO of Google through dedication and intelligence. His humble personality, calm leadership, and focus on innovation that helps people make him truly inspiring.";
+}
